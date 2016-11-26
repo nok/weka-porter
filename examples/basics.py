fe5ad91fee606569674283a163562ec1242b6c04
Darius Morawiec <darius.morawiec@nok.onl>
2016-11-26 23:32:27 +0100

    init stable version

--- examples/basics.py
+++ examples/basics.py
@@ -1,4 +1,31 @@
-from weka_weca import port
+import os
+from weka_weca import Porter
 
-result = port('example.txt')
+porter = Porter(language='java')
+file_path = os.path.join('data', 'weather_data.txt')
+result = porter.port(file_path, method_name='classify')
 print(result)
+
+"""
+public static String classify(String outlook, bool windy, float humidity) {
+    if (outlook == "sunny") {
+        if (humidity <= 75) {
+            return "yes";
+        }
+        else if (humidity > 75) {
+            return "no";
+        }
+    }
+    else if (outlook == "overcast") {
+        return "yes";
+    }
+    else if (outlook == "rainy") {
+        if (windy == true) {
+            return "no";
+        }
+        else if (windy == false) {
+            return "yes";
+        }
+    }
+}
+"""
--- weka_weca/__init__.py
+++ weka_weca/__init__.py
@@ -27,74 +27,149 @@ class Node:
     def __str__(self):
         indent = self.depth * self.indent
         nl = '\n'
-        scope  = nl.join([str(node) for node in self.scope])
+        scope = nl.join([str(node) for node in self.scope])
         result = nl.join([indent + self.start, scope, indent + self.end])
         return result
 
 
-def port(path, method_name='classify'):
-    """
-    Convert a single decision tree as a function.
+class Porter:
 
-    Parameters
-    ----------
-    :param path : string
-        The path of the exported text file.
-    :param method_name : string (default='classify')
-        The method name.
-    :return:
-    """
+    # @formatter:off
+    TEMPLATES = {
+        'c': {
+            'method':   '{return_type} {method_name}({atts}) {{',
+            'if':       'if ({cond}) {{',
+            'elif':     'else if ({cond}) {{',
+            'indent':   '    ',
+        },
+        'java': {
+            'method': 'public static {return_type} {method_name}({atts}) {{',
+            'if':       'if ({cond}) {{',
+            'elif':     'else if ({cond}) {{',
+            'indent':   '    ',
+        },
+        'js': {
+            'method': 'var {method_name} = function({atts}) {{',
+            'if':       'if ({cond}) {{',
+            'elif':     'else if ({cond}) {{',
+            'indent':   '    ',
+        }
+    }
+    # @formatter:on
 
-    # Load data:
-    with open(path, 'r') as file:
-        content = file.readlines()
+    def __init__(self, language='java'):
+        """
+        Parameters
+        ----------
+        :param language : string
+            The target programming language.
+        """
+        self.language = language
 
-    # Create root node:
-    root = Node('', '')
-    atts = []
+    def temp(self, key):
+        try:
+            return self.TEMPLATES.get(self.language).get(key)
+        except IndexError:
+            raise IndexError('Template with key \' %s \' not found.' % key)
 
-    # Construct tree:
-    for line in content:
-        line = line.strip()
-        depth = line.count('|   ')
+    def port(self, path, method_name='classify'):
+        """
+        Convert a single decision tree as a function.
 
-        # Get current node:
-        node = None
-        d = depth
-        if d > 0:
-            while d > 0:
-                node = root.scope[-1] if node is None else node.scope[-1]
-                d -= 1
-        else:
-            node = root.scope
+        Parameters
+        ----------
+        :param path : string
+            The path of the exported text file.
+        :param method_name : string (default='classify')
+            The method name.
+        :return:
+        """
 
-        # Get always the scope list:
-        if type(node) is not list:
-            node = node.scope
+        # Load data:
+        with open(path, 'r') as _file:
+            content = _file.readlines()
 
-        # Build the condition:
-        cond = line[(depth * len('|   ')):]
-        has_return = line.count(':') == 1
-        if has_return:
-            cond = cond.split(':')[0]
-        atts.append(cond.split(' ')[0])
-        cond = Node('if (%s) {' % cond, '}', depth=depth+1)
+        # Create root node:
+        root = Node('', '')
+        return_type = None
+        atts = []
 
-        # Set condition logic:
-        if has_return:
-            indent = cond.indent * (depth + 2)
-            return_value = line[line.find(':') + 1 : line.find('(')].strip()
-            return_value = indent + 'return %s;' % str(return_value)
-            cond.scope.append(return_value)
-        node.append(cond)
+        # Construct tree:
+        for line in content:
+            line = line.strip()
+            depth = line.count('|   ')
 
-    # Merge the relevant attributes:
-    atts = list(set(atts))
-    atts.sort()
-    atts = ', '.join(['float ' + a for a in atts])
+            # Get current node:
+            node = None
+            d = depth
+            if d > 0:
+                while d > 0:
+                    node = root.scope[-1] if node is None else node.scope[-1]
+                    d -= 1
+            else:
+                node = root.scope
 
-    # Wrap function scope around built tree:
-    result = ''.join(['int %s function(%s) {'%
-                      (method_name, atts), str(root), '}'])
+            # Get always the list:
+            if type(node) is not list:
+                node = node.scope
 
-    return result
+            # Build the condition:
+            cond = line[(depth * len('|   ')):].strip()
+            has_return = ':' in cond
+            if has_return:
+                parts = cond.split(':')
+                cond = parts[0].strip()
+                if return_type is None:
+                    return_type = parts[1].strip().split(' ')[0]
+                    if return_type.isdigit():
+                        return_type = 'int'
+                    elif return_type in ['TRUE', 'FALSE']:
+                        return_type = 'bool'
+                    else:
+                        return_type = 'String' \
+                            if self.language in ['java', 'js'] else 'string'
+
+            if ' = ' in cond:
+                cond = cond.replace(' = ', ' == ')
+
+            att_name = cond.split(' ')[0]
+            att_type = 'String' if self.language in ['java', 'js'] else 'string'
+            if any(x in cond for x in ['TRUE', 'FALSE']):
+                att_type = 'bool'
+                cond = cond.replace('TRUE', 'true')
+                cond = cond.replace('FALSE', 'false')
+            elif any(x in cond for x in ['>', '>=', '<', '<=']):
+                att_type = 'float'
+            else:
+                parts = cond.split(' ')
+                parts.append('"%s"' % parts.pop())
+                cond = ' '.join(parts)
+
+            atts.append('%s %s' % (att_type, att_name))
+
+            if_temp = 'if' if len(node) == 0 else 'elif'
+            if_temp = self.temp(if_temp).format(cond=cond)
+            cond = Node(if_temp, '}', depth=depth + 1)
+
+            # Set condition logic:
+            if has_return:
+                indent = cond.indent * (depth + 2)
+                return_value = line[line.find(':') + 1: line.find('(')].strip()
+                if return_type.lower() == 'string':
+                    return_value = '"%s"' % return_value
+                return_value = indent + 'return %s;' % str(return_value)
+                cond.scope.append(return_value)
+            node.append(cond)
+
+        # Merge the relevant attributes:
+        atts = list(set(atts))
+        atts.sort()
+        atts = ', '.join(atts)
+
+        # Wrap function scope around built tree:
+
+        method = self.temp('method').format(
+            return_type=return_type, method_name=method_name, atts=atts)
+        result = ''.join([method, str(root), '}'])
+
+        return result
